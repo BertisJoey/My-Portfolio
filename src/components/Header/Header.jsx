57bfa0338b052426c53725145c2923a2b9d9c013
BertisJoey <joeywiesner@gmail.com>
2023-10-23 23:58:01 -0400

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,2 +1,15 @@
+import { Navbar } from '../Navbar/Navbar';
 
+const Header = () => {
+  return (
+    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
+        <h1 className="m-0" style={{ fontSize: '3rem' }}>
+            Joey Wiesner
+        </h1>
+        <Navbar />
+    </header>
+  );
+};
+
+export default Header;
 
