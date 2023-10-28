db67c075d505c259eb55c452635322adbb4885b3
BertisJoey <joeywiesner@gmail.com>
2023-10-28 16:37:39 -0400

    updated contact page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,11 +3,11 @@ export default function About() {
         <div className = "container pt-4">
             <h1>About Me</h1>
             <p>
-                Hello, my name is Joey Wiesner. I am currently attending a Full-Stack Coding Bootcamp hosted by Michigan State University. I graduated college 5 years ago with a degree in History, and have worked as a social worker for the Department of Health and Human Services ever since. My job has been fulfilling and has given me great experience that I will cherish forever, but it became clear to me in the past couple years that I don't see myself continuing a career as a social worker and that I am looking to branch out.
+                Hello, my name is Joey Wiesner. I am currently attending a Full-Stack Coding Bootcamp hosted by Michigan State University. I graduated college 5 years ago with a degree in History, and have worked as a social worker for the Department of Health and Human Services ever since. My job has been fulfilling and has given me great experience that I will cherish forever, but it became clear to me in the past couple years that I do not see myself continuing a career as a social worker and that I am looking to branch out.
 
                 On this website you should be able to see some of the projects I have worked on during the bootcamp, and I hope to add any future projects that I might be working on. During this bootcamp I have found a new passion and hope to switch careers towards something I see myself growing and developing in.
 
             </p>
         </div>
     )
-};
+}
